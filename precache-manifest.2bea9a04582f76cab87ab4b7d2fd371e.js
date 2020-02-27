816e96e018f30ce3c46dee53b85d40c49719a3c1
Vaernion <vaporz@gmail.com>
2020-02-27 03:45:57 +0100

    Updates

---
+++ precache-manifest.2bea9a04582f76cab87ab4b7d2fd371e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89566e6fd136a3ffd3e1f27a1d8740ec",
+    "url": "/react-test-app/index.html"
+  },
+  {
+    "revision": "3cb856fb96edd0e7ffc3",
+    "url": "/react-test-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c1ce9c1c0ac52093935e",
+    "url": "/react-test-app/static/js/2.8540c2c7.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-test-app/static/js/2.8540c2c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cb856fb96edd0e7ffc3",
+    "url": "/react-test-app/static/js/main.b8f4389f.chunk.js"
+  },
+  {
+    "revision": "53f166583017f3ba953e",
+    "url": "/react-test-app/static/js/runtime-main.92eeb165.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
